de7ab7145775507b1b3cfa6bd5eabf0143a5c5d1
YngLar <YngLarskole@gmail.com>
2017-11-10 21:02:41 +0100

    fkwopafkpoa

--- JS/fade.js
+++ JS/fade.js
@@ -12,3 +12,21 @@ $(window).scroll(function() {
  $(window).scroll(function(){
     $("#coverbilde").css("opacity", 1 - $(window).scrollTop() / $(".parkenbilde").height());
   });
+
+  function åpneDag(evt, dag) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(dag).style.display = "block";
+    evt.currentTarget.className += " active";
+}
